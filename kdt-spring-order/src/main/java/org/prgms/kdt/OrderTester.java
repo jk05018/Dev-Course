66f8c9b1e53ec0b203290b6deda650ed9bbff734
SeungHan <jk05018@naver.com>
2022-04-07 18:43:52 +0900

    Resource 실습 완료

--- kdt-spring-order/src/main/java/org/prgms/kdt/OrderTester.java
+++ kdt-spring-order/src/main/java/org/prgms/kdt/OrderTester.java
@@ -1,9 +1,19 @@
 package org.prgms.kdt;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.prgms.kdt.order.OrderItem;
 import org.prgms.kdt.order.OrderProperties;
@@ -12,16 +22,12 @@ import org.prgms.kdt.voucher.FixedAmountVoucher;
 import org.prgms.kdt.voucher.JdbcVoucherRepository;
 import org.prgms.kdt.voucher.VoucherRepository;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.io.Resource;
 import org.springframework.util.Assert;
 
 public class OrderTester {
-	public static void main(String[] args) {
-		var applicationContext = new AnnotationConfigApplicationContext();
-		applicationContext.register(AppConfiguration.class);
-		var environment = applicationContext.getEnvironment();
-		environment.setActiveProfiles("local"); //여러개의 프로파일을 사용할 수 있다.
-		applicationContext.refresh(); // profile이 적용될 수 있게 refresh?
-		// refresh 하면 default profile이 읽히는데 우리는 dev라는 profile을 지정했으므
+	public static void main(String[] args) throws IOException {
+		var applicationContext = new AnnotationConfigApplicationContext(AppConfiguration.class);
 
 		/*var version = environment.getProperty("kdt.version",String.class);
 		var minOrderAmount = environment.getProperty("kdt.minimum-order-amount",Integer.class);
@@ -40,6 +46,20 @@ public class OrderTester {
 		System.out.println(MessageFormat.format("support-vendors : {0}", orderProperties.getSupportVendors()));
 		System.out.println(MessageFormat.format("description : {0}", orderProperties.getDescription()));*/
 
+		Resource resource = applicationContext.getResource("classpath:application.yaml");
+		Resource resource2 = applicationContext.getResource("file:sample.txt");
+		Resource resource3 = applicationContext.getResource("https://stackoverflow.com/");
+		System.out.println(MessageFormat.format("Resource ->{0}", resource3.getClass().getCanonicalName()));
+		File file = resource2.getFile();
+		List<String> strings = Files.readAllLines(file.toPath()); // file.toiPath() 파일의 경로 지 개행을 기준으로 짤린다
+		System.out.println(strings);
+		System.out.println(strings.stream().collect(Collectors.joining("\n")));
+
+		ReadableByteChannel readableByteChannel = Channels.newChannel(resource3.getURI().toURL().openStream());
+		BufferedReader bufferedReader = new BufferedReader(
+			Channels.newReader(readableByteChannel, StandardCharsets.UTF_8));
+		Stream<String> lines = bufferedReader.lines();
+		System.out.println(lines.collect(Collectors.joining("\n")));
 
 		var customerId = UUID.randomUUID();
 
