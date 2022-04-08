cb113d64e35e1ef4bce6fbd1fede671fdbfcad97
SeungHan <jk05018@naver.com>
2022-04-08 17:33:45 +0900

    logback 실습

--- kdt-spring-order/src/main/java/org/prgms/kdt/order/OrderProperties.java
+++ kdt-spring-order/src/main/java/org/prgms/kdt/order/OrderProperties.java
@@ -3,6 +3,8 @@ package org.prgms.kdt.order;
 import java.text.MessageFormat;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +12,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ConfigurationProperties(prefix = "kdt")
 public class OrderProperties implements InitializingBean {
+
+	private static final Logger logger = LoggerFactory.getLogger(OrderProperties.class);
 
 	private String version;
 
@@ -23,10 +27,10 @@ public class OrderProperties implements InitializingBean {
 
 	@Override
 	public void afterPropertiesSet() throws Exception {
-		System.out.println("OrderProperties yaml start");
-		System.out.printf("version : %s%n", version);
-		System.out.printf("minOrderAmount : %d\n", minimumOrderAmount);
-		System.out.println(MessageFormat.format("support vendors : {0}", supportVendors));
+		logger.debug("OrderProperties yaml start");
+		logger.debug("version : %s%n", version);
+		logger.debug("minOrderAmount : %d\n", minimumOrderAmount);
+		logger.debug(MessageFormat.format("support vendors : {0}", supportVendors));
 	}
 
 	public String getVersion() {
