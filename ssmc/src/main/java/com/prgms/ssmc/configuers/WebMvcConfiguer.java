c956b35bf8c3e79dd40e5d736c3d80201571c835
SeungHan <jk05018@naver.com>
2022-05-17 10:59:35 +0900

    Spring security 기본 설정 mvcCofiguer, securityConfiguer

---
+++ ssmc/src/main/java/com/prgms/ssmc/configuers/WebMvcConfiguer.java
@@ -0,0 +1,17 @@
+package com.prgms.ssmc.configuers;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfiguer implements WebMvcConfigurer {
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		// ㅇㅣ; path로 오면 아 view를 호출하라
+		registry.addViewController("/").setViewName("index");
+		registry.addViewController("/me").setViewName("me");
+	}
+
+}
