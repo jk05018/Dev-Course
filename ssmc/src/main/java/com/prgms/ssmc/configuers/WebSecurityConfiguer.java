a6772f2c7c99b1edff8741afc9ac7420f87276e1
SeungHan <jk05018@naver.com>
2022-05-18 17:44:28 +0900

    security ssl 인증서 적용

--- ssmc/src/main/java/com/prgms/ssmc/configuers/WebSecurityConfiguer.java
+++ ssmc/src/main/java/com/prgms/ssmc/configuers/WebSecurityConfiguer.java
@@ -1,18 +1,47 @@
 package com.prgms.ssmc.configuers;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfiguer extends WebSecurityConfigurerAdapter {
 
 	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser(
+				User.builder()
+					.username("user")
+					.password(passwordEncoder().encode("user123"))
+					.roles("USER")
+					.build()
+			).withUser(
+				User.builder()
+					.username("admin")
+					.password(passwordEncoder().encode("admin123"))
+					.roles("ADMIN")
+					.build()
+			);
+	}
+
+
+	@Bean
+	PasswordEncoder passwordEncoder(){
+		return NoOpPasswordEncoder.getInstance();
+	}
+
+	@Override
 	public void configure(WebSecurity web) {
-		web.ignoring().antMatchers("/assets/**");
+		web.ignoring().antMatchers("/assets/**"); // 필터를 그냥 통화 주로 정적 리소스에 사용
 	}
 
 	@Override
@@ -23,9 +52,21 @@ public class WebSecurityConfiguer extends WebSecurityConfigurerAdapter {
 			.anyRequest().permitAll()
 			.and()
 			.formLogin()
-			.defaultSuccessUrl("/")
-			.permitAll()
+				.defaultSuccessUrl("/")
+				.permitAll()
 			.and()
+			.logout()
+				.logoutUrl("/logout")
+				.logoutSuccessUrl("/")
+			.invalidateHttpSession(true)
+			.clearAuthentication(true)
+			.and()
+			.rememberMe()
+				.rememberMeParameter("remember-me")
+			.tokenValiditySeconds(300)
+			.and()
+			.requiresChannel()
+			.anyRequest().requiresSecure()
 		;
 	}
 }
