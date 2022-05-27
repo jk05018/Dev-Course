d42dc6b9a32119ed6a454c2769b841243d5bbd46
SeungHan <jk05018@naver.com>
2022-05-27 15:05:05 +0900

    jwt finish

--- jwt/src/main/java/com/example/jwt/user/UserRestController.java
+++ jwt/src/main/java/com/example/jwt/user/UserRestController.java
@@ -2,6 +2,8 @@ package com.example.jwt.user;
 
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,8 +31,14 @@ public class UserRestController {
 		final Authentication resultToken = authenticationManager.authenticate(authToken);
 		JwtAuthenticationToken authenticated = (JwtAuthenticationToken)resultToken;
 		JwtAuthentication principal = (JwtAuthentication)authenticated.getPrincipal();
-		final User user = (User)authenticated.getDetails();
+		final User user = (User)authenticated.getDetails();ㅁ
 		return new UserDto(principal.token, principal.username, user.getGroup().getName());
+	}
+
+	@GetMapping("/user/me")
+	public UserDto me(@AuthenticationPrincipal JwtAuthentication authentication){
+		// @AuthenticationPrincipal 어노테이셔을 붙이면 SecurityContext 에 저장되어 있던 AUTHENTIcation에서 Principal을 가져온다?
+
 	}
 
 
